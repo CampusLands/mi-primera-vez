75218eeec181fdc8e71e681d07d1d8f90db00e3a
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-12-03 07:50:07 -0500

    Temas vistos, variables, tipos de datos, listas, entrada y salida de datos

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# VARIABLES
+# TIPOS DE DATOS PRIMITIVOS
+# - String = cadenas de caracteres  'str'
+# - Char = Caracter 'str'
+# - Number = Enteros 'int', Decimal 'float'
+# - Bolean = Datos logicos 'bool'
+name = "Miguel Angel"
+age = 24
+heigth = 1.63
+trainer = True
+
+# CONJUNTOS DE DATOS
+# - Array = Conjuntos de datos de una dimencion 'list'
+hobbies = list()
+hobbies = ["Cocinar", "Programar", "Dormir", "Musica"]
+
+# MODIFICAR UN DATO DE UNA LISTA
+# hobbies[2] = "Despertarme"
+
+# SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
+#dato = input("Ingrese otro pasatiempos: ")
+#hobbies.append(dato)
+#print(hobbies)
+
+# ELIMINAR EL ULTIMO DATO DE LA LISTA
+hobbies.pop()
+print(hobbies)
+
