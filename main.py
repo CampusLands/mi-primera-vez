cb1d8dd819c9a441ffaeeb99e497470a51472ec5
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-12-04 07:10:02 -0500

    Operadores arigmeticos y de comparacion, uso del if, elif. else

--- main.py
+++ main.py
@@ -1,28 +1,76 @@
-# VARIABLES
-# TIPOS DE DATOS PRIMITIVOS
-# - String = cadenas de caracteres  'str'
-# - Char = Caracter 'str'
-# - Number = Enteros 'int', Decimal 'float'
-# - Bolean = Datos logicos 'bool'
-name = "Miguel Angel"
-age = 24
-heigth = 1.63
-trainer = True
+# Libreria matematica
+# import math
 
-# CONJUNTOS DE DATOS
-# - Array = Conjuntos de datos de una dimencion 'list'
-hobbies = list()
-hobbies = ["Cocinar", "Programar", "Dormir", "Musica"]
+# operadores arigmeticos 'Number'
+# + - * / // ** % 
 
-# MODIFICAR UN DATO DE UNA LISTA
-# hobbies[2] = "Despertarme"
+# Operador argimetico Abreviado
+# numero1 = 5
+# numero2 = 5
+# numero1 += numero2
 
-# SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
-#dato = input("Ingrese otro pasatiempos: ")
-#hobbies.append(dato)
-#print(hobbies)
 
-# ELIMINAR EL ULTIMO DATO DE LA LISTA
-hobbies.pop()
-print(hobbies)
+# Operadores de comparacion
+# > == != < >= <= !==
+
+
+# print("Codigo Inicio")
+# edad = int(input("Ingrese su edad: "))
+
+# if(edad <= 5):
+#     print("Usted es un bebe")
+# elif(edad <= 12):
+#     print("Usted es un niñ@")
+# elif(edad <= 16):
+#     print("Usted es un preadolecente")
+# else:
+#     print("Usted es un ....")
+
+# print("Codigo terminado")
+
+# # Operadores logicos
+# # and  or  not
+# # &&   ||  !
+
+# # and
+# True and True = True
+# False and True = False
+# True and False = False
+# False and False = False
+
+# # or
+# True or True = True
+# False or True = True
+# True or False = True
+# False or False = False
+
+# # not
+# not True = False
+# not False = True
+
+
+
+
+
+
+
+
+print("Codigo Inicio")
+edad = int(input("Ingrese su edad: ")) # 23
+
+if(edad == 17): # false
+    print("Puede solicitar un creadito como pasante")
+elif(edad >= 18 and edad < 20): # true and false = false
+    print("Puede solicitar un credito")
+elif(edad >= 20 and edad <= 24): # true and true = true
+    print("Puede solicitar un credito y bono")
+else:
+    print("Las demas edades no pueden solicitar un credito")
+
+
+print("Codigo terminado")
+
+
+
+
 
